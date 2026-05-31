2d2151c6d45f044f136cddc5cee8c255cd1b5b41
slmc-shizuoka <serizawamo@pcdepot.jp>
2026-05-31 12:00:13 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE='storepicker-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  // XLSXライブラリはCDN(別ドメイン)なのでキャッシュ優先しつつネットにフォールバック
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>r)));
+});
